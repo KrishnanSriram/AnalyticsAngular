a0f2487e82a34b9ad48b31b18bee158c603fcb24
krishnansriram <krishnan.sriram@hotmail.com>
2016-08-29 16:54:25 -0400

    Connected ngForm for new component

--- src/app/channels/newchannel/newchannel.component.ts
+++ src/app/channels/newchannel/newchannel.component.ts
@@ -1,19 +1,26 @@
 import {Component, OnInit, Input} from '@angular/core';
+import {FormBuilder, ReactiveFormsModule, NgForm} from "@angular/forms";
 
 @Component({
     moduleId: module.id,
     selector: 'app-newchannel',
     templateUrl: 'newchannel.component.html',
+
 })
 export class NewchannelComponent implements OnInit {
 
     @Input() formid: string;
     @Input() formtitle: string;
 
-    constructor() {
+    constructor(private formBuilder: FormBuilder) {
+        console.log("NewChannelDialog instantiated: " + formBuilder);
     }
 
     ngOnInit() {
+    }
+
+    onSubmit(form: NgForm) {
+        console.dir(form.value);
     }
 
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import {routing} from './home.routes'
 import {ChannelsModule} from "../channels/channels.module";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
-    imports: [CommonModule, routing],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule,routing],
     declarations: [HomeComponent],
     exports: [HomeComponent],
     bootstrap: [HomeComponent],
